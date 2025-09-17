3073091f04ac1edc9195378332dc1f44cf4ef65d
Lukman Hakim <hlukman156@gmail.com>
2025-09-17 23:47:12 +0700

    Add new plugin

--- lua/plugins/indent-line.lua
+++ lua/plugins/indent-line.lua
@@ -1,5 +1,23 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
+	config = function()
+		require("ibl").setup({
+			debounce = 100,
+			whitespace = { highlight = { "Whitespace", "NonText" } },
+			exclude = {
+				filetypes = {
+					'lspinfo',
+					'packer',
+					'checkhealth',
+					'help',
+					'man',
+					'dashboard',
+					'lua',
+					'',
+				}
+			}
+		})
+	end,
 	opts = {} 
 }
